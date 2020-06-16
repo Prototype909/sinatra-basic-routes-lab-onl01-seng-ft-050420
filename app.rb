9a80d17f50e829a12891c707dca47270e52bab8e
Steve Anderson <prototype909@msn.com>
2020-06-16 22:31:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,4 +8,12 @@ class App < Sinatra::Base
   get '/name' do
     "My name is Steve "
   end
+
+  get '/hometown' do
+    "My hometown is Denver"
+  end
+
+  get '/favorite-song' do
+    My favorite song is Hala
+  end
 end
